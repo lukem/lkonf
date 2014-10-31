eca7905cc0228a298cc00f1f33205eeec0385235
Luke Mewburn <luke@mewburn.net>
2014-11-01 03:05:59 +1100

    Extend test harness
    Refactor how tests are added to use a struct mapping name to flag, function.
    Implement initial test for lkonf_load_file() and lkonf_load_string().

--- tests/lkonf_test.c
+++ tests/lkonf_test.c
@@ -11,12 +11,12 @@ test_construct(void)
 {
 	printf("lkonf_construct()\n");
 
-	/* construct lkonf, confirm initial state */
+	/* construct lkonf_t, confirm initial state */
 	{
 		lkonf_t * tc1 = lkonf_construct();
 		assert(tc1 && "lkonf_construct returned 0");
 
-		lkerr_t errcode = lkonf_get_error_code(tc1);
+		const lkerr_t errcode = lkonf_get_error_code(tc1);
 		assert(LK_OK == errcode && "errcode != LK_OK");
 
 		const char * errstr = lkonf_get_error_string(tc1);
@@ -38,7 +38,7 @@ test_destruct(void)
 {
 	printf("lkonf_destruct()\n");
 
-	/* destruct a default lkonf */
+	/* destruct a default lkonf_t */
 	{
 		lkonf_t * tc1 = lkonf_construct();
 		assert(tc1 && "lkonf_construct returned 0");
@@ -55,35 +55,101 @@ test_destruct(void)
 }
 
 
+int
+test_load_file(void)
+{
+	printf("lkonf_load_file()\n");
 
-enum TestMask {
-	TEST_CONSTRUCT		= 1<<0,
-	TEST_DESTRUCT		= 1<<1,
+	/* fail: load null kconf_t: fail */
+	{
+		assert(LK_LKONF_NULL == lkonf_load_file(0, 0));
+	}
+
+/* TODO: fail: load kconf_t with null state */
+
+	/* fail: load with filename null pointer */
+	{
+		lkonf_t * tc1 = lkonf_construct();
+		assert(tc1 && "lkonf_construct returned 0");
+		assert(LK_ARG_BAD == lkonf_load_file(tc1, 0));
+		lkonf_destruct(tc1);
+	}
+
+/* TODO: finish */
+
+	return EXIT_SUCCESS;
+}
+
+
+int
+test_load_string(void)
+{
+	printf("lkonf_load_string()\n");
+
+	/* fail: load null kconf_t: fail */
+	{
+		assert(LK_LKONF_NULL == lkonf_load_string(0, 0));
+	}
+
+/* TODO: fail: load kconf_t with null state */
+
+	/* fail: load with string null pointer */
+	{
+		lkonf_t * tc1 = lkonf_construct();
+		assert(tc1 && "lkonf_construct returned 0");
+		assert(LK_ARG_BAD == lkonf_load_string(tc1, 0));
+		lkonf_destruct(tc1);
+	}
+
+/* TODO: finish */
+
+	return EXIT_SUCCESS;
+}
+
+
+/**
+ * One flag per test.
+ */
+enum TestFlags {
+	TF_construct		= 1<<0,
+	TF_destruct		= 1<<1,
+	TF_load_file		= 1<<2,
+	TF_load_string		= 1<<3,
 };
 
-void
-do_test(
-	const enum TestMask	iTests,
-	const enum TestMask	iFlag, 
-	int 			(*iFunc)(void),
-	int *			ioResult)
+
+/**
+ * Mapping of test name to TestFlags and function to execute.
+ */
+const struct {
+	const char *		name;
+	enum TestFlags		flag;
+	int			(*function)(void);
+} nameToTest[] = {
+	{ "construct",		TF_construct,	test_construct },
+	{ "destruct",		TF_destruct,	test_destruct },
+	{ "load_file",		TF_load_file,	test_load_file },
+	{ "load_string",	TF_load_string,	test_load_string,},
+	{ 0,			0,		0 },
+};
+
+
+int
+usage(const char * progname)
 {
-	if (! (iTests & iFlag)) {
-		return;
+	fprintf(stderr, "Usage: %s <test> [...]\n", progname);
+	fprintf(stderr, " Supported <test> values:\n");
+	fprintf(stderr, "   all          all tests\n");
+	int ti;
+	for (ti = 0; nameToTest[ti].name; ++ti) {
+		fprintf(stderr, "   %s\n", nameToTest[ti].name);
 	}
-
-	int res = iFunc();
-	if (res > *ioResult) {
-		*ioResult = res;
-	}
-
+	return EXIT_FAILURE;
 }
 
 int
 main(int argc, char * argv[])
 {
-	enum TestMask	tests = 0;
-
 		/* determine progname */
 	char * progname = strrchr(argv[0], '/');
 	if (progname) {
@@ -92,33 +158,43 @@ main(int argc, char * argv[])
 		progname = argv[0];
 	}
 
-		/* no arguments: test everything */
-	if (argc == 1) {
-		tests = ~0;
-	}
-
 		/* only test specific items */
-	int i = 0;
-	for (i = 1; i < argc; ++i) {
-		if (0) {
-		} else if (0 == strcmp("construct", argv[i])) {
-			tests |= TEST_CONSTRUCT;
-		} else if (0 == strcmp("destruct", argv[i])) {
-			tests |= TEST_DESTRUCT;
-		} else {
-			fprintf(stderr, "%s: unknown argument '%s'\n",
-				progname, argv[i]);
-			return EXIT_FAILURE;
+	enum TestFlags tests = 0;
+	int ai, ti;
+	for (ai = 1; ai < argc; ++ai) {
+		if (0 == strcmp("all", argv[ai])) {
+			tests = ~0;
+			continue;
 		}
+		for (ti = 0; nameToTest[ti].name; ++ti) {
+			if (0 == strcmp(argv[ai], nameToTest[ti].name))
+				break;
+		}
+		if (! nameToTest[ti].name) {
+			fprintf(stderr, "%s: unknown argument '%s'\n",
+				progname, argv[ai]);
+			return usage(progname);
+		}
+		tests |= nameToTest[ti].flag;
 	}
 
-	assert(tests && "no tests selected");
+	if (! tests) {
+		fprintf(stderr, "%s: no tests selected\n", progname);
+		return usage(progname);
+	}
 
 		/* perform each test */
 	int result = EXIT_SUCCESS;
 
-	do_test(tests, TEST_CONSTRUCT, test_construct, &result);
-	do_test(tests, TEST_DESTRUCT, test_destruct, &result);
+	for (ti = 0; nameToTest[ti].name; ++ti) {
+		if (! (tests & nameToTest[ti].flag)) {
+			continue;
+		}
+		int res = nameToTest[ti].function();
+		if (res > result) {
+			result = res;
+		}
+	}
 
 	return result;
 }
